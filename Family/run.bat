e2fa47da8d748e3c7e9e3f8a0c0df0df20c247ec
Bennet Sunder <ben.sunder@gmail.com>
2019-07-05 04:24:46 +0530

    updated batch script

--- Family/run.bat
+++ Family/run.bat
@@ -1,2 +1,5 @@
+"%ProgramFiles%\dotnet\dotnet.exe" clean
+"%ProgramFiles%\dotnet\dotnet.exe" build
+"%ProgramFiles%\dotnet\dotnet.exe" test ../Family.Tests
 "%ProgramFiles%\dotnet\dotnet.exe" run --project ../Family
 PAUSE
